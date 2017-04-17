8bc3ea9cd50a0a765b9a9d18f977ec5c81583d65
souleex <souleex@gmail.com>
2017-04-17 15:01:57 -0700

    finished Angular Pipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,20 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'mw-app',
     templateUrl: 'app/app.component.html',
-	styleUrls: ['app/app.component.css']
+    styleUrls: ['app/app.component.css']
 })
-export class AppComponent {
-    onMediaItemDelete(mediaItem) {
-        
-    }
-    
-    firstMediaItem = {
-        id:1,
-        name: "Firebug",
-        medium: "Series",
-        category: "Sci-Fi",
-        year: 2010,
-        watchedOn: 1294166565384,
-        isFavorite: false
-    };
-}
+export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
     imports: [
@@ -9,7 +13,10 @@ import { MediaItemComponent } from './media-item.component';
     ],
     declarations: [
         AppComponent,
-		MediaItemComponent
+        MediaItemComponent,
+        MediaItemListComponent,
+        FavoriteDirective,
+        CategoryListPipe
     ],
     bootstrap: [
         AppComponent
